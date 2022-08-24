e1dd10908aed63e8663cdea53b7e4429581dbd17
tharsila <tharsilapaula@gmail.com>
2022-08-24 04:41:26 -0300

    feat: task delete

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,30 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
-import { FaBeer } from 'react-icons/fa';
+import { ITask } from './interfaces/Task';
 
 function App() {
-  
+  const[taskList, setTaskList] = useState<ITask[]>([])
+
+  const deleteTask = (id:number) => {
+    setTaskList(
+      taskList.filter((task) => {
+        return task.id !== id
+      })
+    )
+  }
+
   return (
     <>
       <Header/>
       <C.CardContainer>
         <div>
           <h2>Insira sua próxima tarefa:</h2>
-          <TaskForm btnText='Criar Tarefa'/>
+          <TaskForm btnText='Criar Tarefa' taskList={taskList} setTaskList={setTaskList}/>
         </div>
         <div>
           <h2>Lista de tarefas:</h2>
-          <TaskList/>
+          <TaskList taskList={taskList} handleDelete={deleteTask}/>
         </div>
       </C.CardContainer>
       <Footer/>
